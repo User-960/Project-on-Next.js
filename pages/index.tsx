647ec577401dd2e57321103e62361c5689a6316a
Ivan <ivanvolodin960@gmail.com>
2023-06-22 12:56:51 +0300

    feat: add title and text in component Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
 import Layout from "../components/Layout/Layout";
 
-export default function Home() {
+export default function Home({ name }: any) {
   return (
     <>
       <Layout>
-        Контент для главной страницы
+        <h1>Главная страница</h1>
+
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam ducimus aspernatur consequatur nobis odio labore, cupiditate architecto! Vero quisquam consequuntur perspiciatis accusamus. Similique dolores voluptate impedit at deserunt vitae repellat.
+
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis nisi repellendus, atque corrupti facilis, ipsa adipisci vel fuga nam tempora minima, quaerat assumenda! Nisi, error laudantium esse ducimus quam quis.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae fugiat itaque officiis ad. Soluta dolores autem adipisci corporis minima deleniti, voluptas unde dolor excepturi cumque ducimus! Quos inventore esse deserunt.
+        </p>
       </Layout>
     </>
   )
